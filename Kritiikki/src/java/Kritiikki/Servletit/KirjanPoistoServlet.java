51b007cc041168a1b9dcbd057d097f4f20d612d1
tiikkala <tapioikkala@gmail.com>
2016-04-25 19:02:22 +0300

    muokkaus ja poisto valmiit

---
+++ Kritiikki/src/java/Kritiikki/Servletit/KirjanPoistoServlet.java
@@ -0,0 +1,70 @@
+package Kritiikki.Servletit;
+
+import Kritiikki.Mallit.Kirja;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Toteuttaa kirjan poistamiseen liittyvän toiminnallisuuden.
+ */
+public class KirjanPoistoServlet extends YleisServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = luoPrintWriter(response);
+        try {
+            int id = haeIdSessionilta(request);
+            new Kirja().poistaKirja(id);
+            String sivu = "Kirja?id=" + id;
+            ohjaaSivulle("Etusivu", response);
+            request.getSession().setAttribute("ilmoitus", "Kirjan poistaminen onnistui.");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
